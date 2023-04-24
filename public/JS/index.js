b90b00100831b889b9aa942e3440634a2085d5ed
Tayro <adventuregamr@gmail.com>
2023-04-24 10:58:33 +0200

    Oppdaterende tall og besøksliste

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,20 +1,29 @@
 const nameInsertForm = document.querySelector("form#nameInsertForm");
 const welcomeMsg = document.querySelector("div#welcomeMsg");
+const counter = document.querySelector("div#counter h1#number");
+const peopleList = document.querySelector("ul#peopleList");
 
 let remainSec;
+let peopleArray = [];
 
 function updateClock(){
     let date = new Date();
     remainSec = 60 - date.getSeconds().toString();
-    console.log("Counting...", date.getSeconds().toString(), remainSec, date.getMinutes().toString());
+    //console.log("Counting...", date.getSeconds().toString(), remainSec, date.getMinutes().toString());
 
     if(remainSec == 60){
         console.clear();
         console.log("New minute");
+        genNum();
     }
 }
 
 setInterval(updateClock, 1000);
+
+function genNum(){
+    let num = Math.floor(Math.random() * 100);
+    counter.innerHTML = num;
+}
 
 if(sessionStorage.getItem("TypedName")){
     let name = sessionStorage.getItem("TypedName");
@@ -24,9 +33,21 @@ if(sessionStorage.getItem("TypedName")){
     welcomeMsg.innerHTML = template;
 }
 
+if(localStorage.getItem("Names")){
+    peopleArray = JSON.parse(localStorage.getItem("Names"))
+    peopleArray.forEach(person => {
+        let template = `
+            <li>${person}</li>
+        `;
+        peopleList.innerHTML += template;
+    });
+}
+
 nameInsertForm.addEventListener("submit", (e) => {
     const name = nameInsertForm.nameInsert.value;
-    localStorage.setItem("TypedName", name); //REdo this
+    //localStorage.setItem("TypedName", name); //REdo this
+    peopleArray.push(name);
+    localStorage.setItem("Names", JSON.stringify(peopleArray));
     sessionStorage.setItem("TypedName", name);
 
     let template = `
@@ -35,4 +56,5 @@ nameInsertForm.addEventListener("submit", (e) => {
     welcomeMsg.innerHTML = template;
 })
 
+genNum();
 updateClock();
